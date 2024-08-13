29ec73533161fe340f468b2f5150e350f5efb6e3
Dansouzahhh <146783999+Dansouzahhh@users.noreply.github.com>
2024-08-13 16:25:29 -0300

    Feitoupload_EX123

---
+++ EX_02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, R; /* Variavel INT = Inteiro */
+
+            Console.WriteLine("entre o 1o valor: ");
+
+            A = int.Parse(Console.ReadLine()); /*  são usados para converter uma string em um valor de um tipo primitivo  */
+
+            Console.WriteLine("Entre o 2o. valor "); /*  O console no C# é uma interface que permite a interação entre o usuário e o programa através de texto */
+            B = int.Parse(Console.ReadLine()); /* Ele ira converte Texto em Números inteiros */
+
+            R = A = B; /* Atribuição em Cadeia */ 
+
+            Console.WriteLine();
+            Console.WriteLine("resultado = " + R); 
+            
+            Console.WriteLine();
+            Console.Write("Tecla <Enter> para continuar... "); /* O Console.Write é usado para escrever texto no console sem pular para a próxima linha */
+
+            Console.ReadLine(); /* O Console.ReadLine() é uma maneira de ler o que o usuário digita no console */
+
+        }
+    }
+}
---
+++ Ex_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01 /* Nome do Projeto "Espaço" */
+{
+    internal class Program /* Classe interna */
+    {
+        static void Main(string[] args)  /* Ponto de entrada */
+        {
+            string NOME; /* String seria um Literal  */
+
+            Console.WriteLine("entre seu nome: "); 
+            NOME = Console.ReadLine(); 
+            Console.WriteLine("Ola, " + NOME); /* Ele escrevera sem quebarar a linha e Mostrara o OLa com o Nome digitado  */
+
+            Console.WriteLine(); /* Escrever Sem Quebrar Linha */
+            Console.WriteLine("Tecle <Enter> para encerrar... ");
+            Console.ReadLine();  /* O Console.ReadLine() é uma maneira de ler o que o usuário digita no console */
+
+        }
+    }
+}
